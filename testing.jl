ceb87224250e0d0e4bd2edbab73698827bc2f899
atulchin <8276400+atulchin@users.noreply.github.com>
2024-06-06 21:01:16 -0400

    plots

--- testing.jl
+++ testing.jl
@@ -98,33 +98,33 @@ plotk(ns::Int,nr::Int) = [:legend=>nothing, :bg=>"#e1e4e1",
     :ls=>repeat([:dashdot :solid :dash :dashdot][:,1:ns], inner=(1,nr))]
 P(a,b)::Float64 = iszero(b) ? 0.0 : a/b
 
-r_inc = 5
-ids = 2:4
-xlim = floor(Int, 600/r_inc)
-xs = collect(1:xlim) .* r_inc;
-test_time = Int(maximum(t_closed)/r_inc)
-
 cbg_idxs = let k = dser_path("jlse/cbg_idxs.jlse"); Dict(String(v)=>Int(i) for (i,v) in k) end
 pop_by_cbg = let d = dser_path("precalc/p_idxs_all_by_h_cbg.jlse"); Dict(String(k)=>length(v) for (k,v) in d) end
 pop_by_cbgidx = Dict(v=>pop_by_cbg[k] for (k,v) in cbg_idxs)
 pops_all = [pop_by_cbgidx[i] for i in 1:maximum(values(cbg_idxs))]
 
 
-g_open = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_agents_assigned,1:20);
-g_wp_sch = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_agents_assigned,1:20);
-
-total_open = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:cumI,1:20);
-total_wp_sch = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:cumI,1:20);
-## time when open has the same cumI
-test_time_o = argmin(abs.(vec(mean(total_open;dims=2)) .- mean(total_wp_sch[test_time,:])))
 
 
+r_inc = 5
+ids = 2:4
+xlim = floor(Int, 600/r_inc)
+xs = collect(1:xlim) .* r_inc;
+test_time = 30#Int(maximum(t_closed)/r_inc)
+pI = 0.1
 
-g_open_t = g_open[test_time_o,:]
+## time when open has the same cumI?
+#total_open = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:cumI,1:20);
+#total_wp_sch = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:cumI,1:20);
+#test_time_o = argmin(abs.(vec(mean(total_open;dims=2)) .- mean(total_wp_sch[test_time,:])))
+
+g_open_t = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_agents_assigned,1:20)[test_time,:];
+g_wp_sch_t = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_agents_assigned,1:20)[test_time,:];
 g_open_p = [g_open_t[r] ./ pops_all for r in eachindex(g_open_t)]
-
-g_wp_sch_t = g_wp_sch[test_time,:]
 g_wp_sch_p = [g_wp_sch_t[r] ./ pops_all for r in eachindex(g_wp_sch_t)]
+
+total_p_open = [sum(g_open_t[r]) / sum(pops_all) for r in eachindex(g_open_t)]
+total_p_wp_sch = [sum(g_wp_sch_t[r]) / sum(pops_all) for r in eachindex(g_wp_sch_t)]
 
 
 ## baltimore
@@ -133,28 +133,26 @@ g_wp_sch_p = [g_wp_sch_t[r] ./ pops_all for r in eachindex(g_wp_sch_t)]
     mask = (t.COUNTYFP .== "510")
     (t.geometry[mask], t.GEOID[mask])
 end;
+placename = "balt"
 
 ## dc
 (shapes, cbg_codes) = let 
     t = Shapefile.Table("tl_2019_11_bg/tl_2019_11_bg.shp")
     (t.geometry, t.GEOID)
 end;
-
+placename = "dc"
 
 idxs = [get(cbg_idxs,k,-1) for k in cbg_codes]
 #pops = [get(pop_by_cbg,k,0) for k in cbg_codes]
 
-
-#g = mean(g_open_p); col_range = (0,0.4)
-#g = sqrt.(var(g_open_p)); col_range = (0,0.1)
-g = mean(g_wp_sch_p); col_range = (0,0.4)
-#g = sqrt.(var(g_wp_sch_p)); col_range = (0,0.1)
+g = mean(g_open_p ./ total_p_open); col_range = (0,1.75); plotname = placename * "_open"
+#g = sqrt.(var(g_open_p ./ total_p_open)); col_range = (0,1.0); plotname = placename * "_sd_open"
+#g = mean(g_wp_sch_p ./ total_p_wp_sch); col_range = (0,1.75); plotname = placename * "_closed"
+#g = sqrt.(var(g_wp_sch_p ./ total_p_wp_sch)); col_range = (0,1.0); plotname = placename * "_sd_closed"
 z = reshape([get(g,i,0) for i in idxs], 1, :)
 
-
-let k=:inferno; plot(shapes, clims=col_range, fill=palette(k), fill_z=z, linecolor=palette(k), line_z=z, size=(800,800)) end
-#let k=:inferno; plot(shapes, fill=palette(k), fill_z=z, linecolor=palette(k), line_z=z, size=(800,800)) end
-
+let k=:inferno; plot(shapes, clims=col_range, fill=palette(k), fill_z=z, linecolor=palette(k), line_z=z, size=(800,800), dpi=300) end
+savefig(plotname*".png")
 
 
 
@@ -181,82 +179,84 @@ end
 
 
 ## baltimore
-(shapes, cbg_codes) = let 
+(shapes, cbg_codes1) = let 
     t = Shapefile.Table("tl_2019_24_bg/tl_2019_24_bg.shp"); 
     mask = (t.COUNTYFP .== "510")
     (t.geometry[mask], t.GEOID[mask])
 end;
+cbg_codes = cbg_codes1
 
 ## dc
-(shapes, cbg_codes) = let 
+(shapes, cbg_codes2) = let 
     t = Shapefile.Table("tl_2019_11_bg/tl_2019_11_bg.shp")
     (t.geometry, t.GEOID)
 end;
+cbg_codes = cbg_codes2
+
+## balt+dc
+cbg_codes = [cbg_codes1; cbg_codes2]
+## whole pop
+cbg_codes = collect(keys(cbg_idxs))
 
 
 idxs = filter(i->i>0, [get(cbg_idxs,k,-1) for k in cbg_codes])
 
-cI_b = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_race_black_alone,1:20)[test_time,:];
-cI_w = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_white_non_hispanic,1:20)[test_time,:];
-cI_h = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_hispanic,1:20)[test_time,:];
+f(v)=sum(v[idxs])
+cI_b = f.(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_race_black_alone,1:20));
+cI_w = f.(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_white_non_hispanic,1:20));
+cI_h = f.(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_hispanic,1:20));
 
-pI_b = [sum(cI_b[r][idxs]) / sum(n_b[idxs]) for r in eachindex(cI_b)]
-pI_w = [sum(cI_w[r][idxs]) / sum(n_w[idxs]) for r in eachindex(cI_w)]
-pI_h = [sum(cI_h[r][idxs]) / sum(n_h[idxs]) for r in eachindex(cI_h)]
+oI_b = f.(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_race_black_alone,1:20));
+oI_w = f.(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_white_non_hispanic,1:20));
+oI_h = f.(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_hispanic,1:20));
 
-mean(pI_b), sqrt(var(pI_b))
-mean(pI_w), sqrt(var(pI_w))
-mean(pI_h), sqrt(var(pI_h))
+cI_b_p = cI_b ./ sum(n_b[idxs])
+cI_w_p = cI_w ./ sum(n_w[idxs])
+cI_h_p = cI_h ./ sum(n_h[idxs])
 
-24.06 / 19.0
-(24.06 - 19.0) / 19.0
-29.2 / 19.0
-(29.2 - 19.0) / 19.0
+oI_b_p = oI_b ./ sum(n_b[idxs])
+oI_w_p = oI_w ./ sum(n_w[idxs])
+oI_h_p = oI_h ./ sum(n_h[idxs])
 
+rr_c_h = cI_h_p ./ cI_w_p
+rr_c_b = cI_b_p ./ cI_w_p
 
-
-cI_b = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_race_black_alone,1:20)[test_time_o,:];
-cI_w = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_white_non_hispanic,1:20)[test_time_o,:];
-cI_h = read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_hispanic,1:20)[test_time_o,:];
-
-pI_b = [sum(cI_b[r][idxs]) / sum(n_b[idxs]) for r in eachindex(cI_b)]
-pI_w = [sum(cI_w[r][idxs]) / sum(n_w[idxs]) for r in eachindex(cI_w)]
-pI_h = [sum(cI_h[r][idxs]) / sum(n_h[idxs]) for r in eachindex(cI_h)]
-
-mean(pI_b), sqrt(var(pI_b))
-mean(pI_w), sqrt(var(pI_w))
-mean(pI_h), sqrt(var(pI_h))
-
-
-25.8 / 22.3
-(25.8 - 22.3) / 22.3
-29.3 / 22.3
-(29.3 - 22.3) / 22.3
+rr_o_h = oI_h_p ./ oI_w_p
+rr_o_b = oI_b_p ./ oI_w_p
 
 
 
+#plot([mean(rr_o_h[10:end,:];dims=2) mean(rr_c_h[10:end,:];dims=2)], 
+#    ribbon=[2.0*sqrt.(var(rr_o_h[10:end,:];dims=2)) 2.0*sqrt.(var(rr_c_h[10:end,:];dims=2))])
 
-cI_b = mean(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_race_black_alone,1:20);dims=2)
-cI_w = mean(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_white_non_hispanic,1:20);dims=2)
-cI_h = mean(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_wp_sch_closed",xlim,ids,:geo_hispanic,1:20);dims=2)
+#plot([mean(rr_o_b[10:end,:];dims=2) mean(rr_c_b[10:end,:];dims=2)], 
+#    ribbon=[2.0*sqrt.(var(rr_o_b[10:end,:];dims=2)) 2.0*sqrt.(var(rr_c_b[10:end,:];dims=2))])
+xs = 5 .* collect(10:120)
 
-cI_b_p = [sum(cI_b[i][idxs]) for i in eachindex(cI_b)] ./ sum(n_b[idxs])
-cI_w_p = [sum(cI_w[i][idxs]) for i in eachindex(cI_w)] ./ sum(n_w[idxs])
-cI_h_p = [sum(cI_h[i][idxs]) for i in eachindex(cI_h)] ./ sum(n_h[idxs])
+rr_open = rr_o_b[10:end,:]
+rr_closed = rr_c_b[10:end,:]
+ymax = 1.37
+figname = "rr_race_black"
 
-plot(hcat((cI_b_p ./ cI_w_p), (cI_h_p ./ cI_w_p)))
+rr_open = rr_o_h[10:end,:]
+rr_closed = rr_c_h[10:end,:]
+ymax = 2.05
+figname = "rr_hispanic"
+
+plot(xs,[mean(rr_open;dims=2) mean(rr_closed;dims=2)], 
+linecolor=[:black :black], linestyle=[:dash :solid],
+ylim=(1.0,ymax),
+label=["open" "closed"],ylabel="relative risk",xlabel="day",
+dpi=300)
+
+plot!(xs,[percentile.(eachrow(rr_open),10) percentile.(eachrow(rr_closed),10)], 
+    fillrange=[percentile.(eachrow(rr_open),90) percentile.(eachrow(rr_closed),90)],
+    alpha=0.3,linewidth=0,fillcolor=[:gray55 :gray33],
+    label="")
+
+savefig(figname*".png")
 
 
-cI_b = mean(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_race_black_alone,1:20);dims=2)
-cI_w = mean(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_white_non_hispanic,1:20);dims=2)
-cI_h = mean(read_sums("test/sim_MD_p0"*string(round(Int,pI*100))*"_open",xlim,ids,:geo_hispanic,1:20);dims=2)
-
-cI_b_p = [sum(cI_b[i][idxs]) for i in eachindex(cI_b)] ./ sum(n_b[idxs])
-cI_w_p = [sum(cI_w[i][idxs]) for i in eachindex(cI_w)] ./ sum(n_w[idxs])
-cI_h_p = [sum(cI_h[i][idxs]) for i in eachindex(cI_h)] ./ sum(n_h[idxs])
-
-
-plot(hcat((cI_b_p ./ cI_w_p), (cI_h_p ./ cI_w_p)))
 
 
 
@@ -279,34 +279,128 @@ let z=pI; k=:inferno; plot(shapes, fill=palette(k), fill_z=z, linecolor=palette(
 
 
 
-## initial infections
 
-p_by_cbg = dser_path("precalc/p_idxs_all_by_h_cbg.jlse")
-I0 = UInt32.(first(p_by_cbg["240230006001"], 100))
 
-for i in 2:3
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_iloc"*string(i)*".jlse", 
+
+## network shape
+using Graphs
+
+#netw = sparse(Symmetric(dser_path("jlse/adj_mat_non_hh.jlse"))) .| sparse(Symmetric(dser_path("jlse/adj_mat_hh.jlse")));
+#dum_idxs = Set(keys(dser_path("jlse/adj_dummy_keys.jlse")));
+#outw_idxs = Set(keys(dser_path("jlse/adj_out_workers.jlse")));
+#mask = setdiff(axes(netw,2), dum_idxs, outw_idxs)
+#mu = mean(sum(netw[:,mask], dims=1))
+#nn = size(netw,1)
+#netw, dum_idxs, outw_idxs, mask = nothing, nothing, nothing, nothing
+#GC.gc()
+mu = 8.655965228627515 #7.793946656329233 ## MD
+nn = 9424031 #9440777 ## MD
+nlocs = 2141
+
+
+
+for i in 1:10
+    serialize("test/sim_MD_p0"*string(round(Int,pI*100))*"_base"*string(i)*".jlse", 
     test(t; init_inf=I0, p_inf=pI, p_inf_hh=pI, p_inf_loc=pI,
-    distr_fn_hh=:const, distr_fn_non_hh=:const, distr_params_hh=(6,), distr_params_non_hh=(8,), 
-    distr_fn_loc_res=:exp, distr_params_loc_res=(2,), distr_fn_loc_work=:zero, distr_params_loc_work=() 
-    ))
-    GC.gc()
-end
-
-for i in 1:3
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_h202_iloc"*string(i)*".jlse", 
-    test(t; init_inf=I0, p_inf=pI, p_inf_hh=pI, p_inf_loc=pI, holiday_time=202:223,
-    distr_fn_hh=:const, distr_fn_non_hh=:const, distr_params_hh=(6,), distr_params_non_hh=(8,), 
-    distr_fn_loc_res=:exp, distr_params_loc_res=(2,), distr_fn_loc_work=:zero, distr_params_loc_work=() 
+    distr_fn_hh=:all, distr_fn_non_hh=:all, distr_params_hh=(), distr_params_non_hh=() 
     ))
     GC.gc()
 end
 
 
+## barabasi_albert "scale free" network -- lots of hubs
+for i in 1:10
+    serialize("test/sim_MD_p0"*string(round(Int,pI*100))*"_BA"*string(i)*".jlse", 
+    test(t; init_inf=I0, p_inf=pI, p_inf_hh=pI, p_inf_loc=pI,
+    netw_hh = Bool.(Graphs.LinAlg.adjacency_matrix( barabasi_albert(nn, 4) )),
+    netw_wp = spzeros((nn,nn)),
+    dummies = Set(Int[]),
+    out_workers = Set(Int[]),
+    netw_sch = spzeros((nn,nn)),
+    netw_gq = spzeros((nn,nn)),
+    loc_matrix_res = spzeros((nn,nlocs)),
+    loc_matrix_work = spzeros((nn,nlocs)),
+    loc_lookup_res = Dict(1:nn .=> 0),
+    loc_lookup_work  = Dict(1:nn .=> 0),
+    geo_lookup = Dict(1:nn .=> 0),
+    essential_workers = Set(Int[]),
+    k12_workers = Set(Int[]),
+    race_black_alone = Set(Int[]),
+    white_non_hispanic = Set(Int[]),
+    hispanic = Set(Int[]),
+    mean_hh_connections = mu,
+    mean_wp_connections = 0.0,
+    distr_fn_hh=:all, distr_fn_non_hh=:all, distr_params_hh=(), distr_params_non_hh=() 
+    ))
+    GC.gc()
+end
+
+## erdos_renyi "random" network
+for i in 1:10
+    serialize("test/sim_MD_p0"*string(round(Int,pI*100))*"_ER"*string(i)*".jlse", 
+    test(t; init_inf=I0, p_inf=pI, p_inf_hh=pI, p_inf_loc=pI,
+    netw_hh = Bool.(Graphs.LinAlg.adjacency_matrix( erdos_renyi(nn,round(Int,0.5*mu*nn)) )),
+    netw_wp = spzeros((nn,nn)),
+    dummies = Set(Int[]),
+    out_workers = Set(Int[]),
+    netw_sch = spzeros((nn,nn)),
+    netw_gq = spzeros((nn,nn)),
+    loc_matrix_res = spzeros((nn,nlocs)),
+    loc_matrix_work = spzeros((nn,nlocs)),
+    loc_lookup_res = Dict(1:nn .=> 0),
+    loc_lookup_work  = Dict(1:nn .=> 0),
+    geo_lookup = Dict(1:nn .=> 0),
+    essential_workers = Set(Int[]),
+    k12_workers = Set(Int[]),
+    race_black_alone = Set(Int[]),
+    white_non_hispanic = Set(Int[]),
+    hispanic = Set(Int[]),
+    mean_hh_connections = mu,
+    mean_wp_connections = 0.0,
+    distr_fn_hh=:all, distr_fn_non_hh=:all, distr_params_hh=(), distr_params_non_hh=() 
+    ))
+    GC.gc()
+end
+
+## watts_strogatz "small world" network -- doesn't form hubs
+#sw_net = Bool.(Graphs.LinAlg.adjacency_matrix( watts_strogatz(nn,8,0.25) ));
 
 
 
 
+pI = 0.15
+r_inc = 5
+ids = 2:4
+xlim = floor(Int, 550/r_inc)
+xs = collect(1:xlim) .* r_inc;
+
+
+
+
+n_cI = read_sums("netw_shape/sim_MD_p0"*string(round(Int,pI*100))*"_base",xlim,ids,:cumI,1:10);
+ba_cI = read_sums("netw_shape/sim_MD_p0"*string(round(Int,pI*100))*"_BA",xlim,ids,:cumI,1:10);
+er_cI = read_sums("netw_shape/sim_MD_p0"*string(round(Int,pI*100))*"_ER",xlim,ids,:cumI,1:10);
+
+
+f(x) = 2.0*sqrt.(var(x,dims=2))
+plot(xs,[mean(ba_cI,dims=2) mean(n_cI,dims=2) mean(er_cI,dims=2)],
+    ribbon=[f(ba_cI) f(n_cI) f(er_cI)],
+    linecolor=:black,
+    fillcolor=:gray60,
+    linestyle=[:dash :solid :dashdot],
+    label=["scale free" "synth. pop." "random netw."],
+    ylabel="cumulative infections",xlabel="day",
+    dpi=300)
+
+savefig("network_shape.png")
+
+
+
+
+
+
+
+## old network tests
 
 ## generate random one-time contacts, preserving # contacts per person
 #for i in 1:2
@@ -315,18 +409,6 @@ end
 #    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_rnet"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI, full_net=randonet))
 #    GC.gc()
 #end
-
-#netw = sparse(Symmetric(dser_path("jlse/adj_mat.jlse"))) .| sparse(Symmetric(dser_path("jlse/hh_adj_mat.jlse")));
-#dum_idxs = Set(keys(dser_path("jlse/adj_dummy_keys.jlse")));
-#outw_idxs = Set(keys(dser_path("jlse/adj_out_workers.jlse")));
-#mask = setdiff(axes(netw,2), dum_idxs, outw_idxs)
-#mu = mean(sum(netw[:,mask], dims=1))
-#nn = size(netw,1)
-#netw, dum_idxs, outw_idxs, mask = nothing, nothing, nothing, nothing
-#GC.gc()
-mu = 7.793946656329233 ## MD
-nn = 9440777 ## MD
-
 ## generate random one-time contacts, preserving only the mean # contacts per person
 #for i in 1:2
 #    really_rand = randnet(nn, mu, 1.0)
@@ -335,69 +417,7 @@ nn = 9440777 ## MD
 #    GC.gc()
 #end
 
-using Graphs
 
-## watts_strogatz "small world" network -- doesn't form hubs
-for i in 1:2
-    sw_net = Bool.(Graphs.LinAlg.adjacency_matrix( watts_strogatz(nn,8,0.25) ));
-    GC.gc()
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_sw"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI, full_net=sw_net, dummies = Set(Int64[]), out_workers = Set(Int64[])))
-    GC.gc()
-end
-
-## barabasi_albert "scale free" network -- lots of hubs
-for i in 1:2
-    ba_net = Bool.(Graphs.LinAlg.adjacency_matrix( barabasi_albert(nn, 4) ));
-    GC.gc()
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_ba"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI, full_net=ba_net, dummies = Set(Int64[]), out_workers = Set(Int64[])))
-    GC.gc()
-end
-
-## erdos_renyi "random" network
-for i in 1:2
-    er_net = Bool.(Graphs.LinAlg.adjacency_matrix( erdos_renyi(nn,round(Int,0.5*mu*nn)) ));
-    GC.gc()
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_er"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI, full_net=er_net, dummies = Set(Int64[]), out_workers = Set(Int64[])))
-    GC.gc()
-end
-
-
-## swap out files...
-## read network from jlse dir, run with above params, save output
-for i in 1:3
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_NO_INC"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI))
-    GC.gc()
-end
-
-
-## lower init
-t = 602
-I0 = [2=>10,3=>0,4=>0]
-pI = 0.15
-## swap out files...
-## read network from jlse dir, run with above params, save output
-for i in 1:3
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_I10"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI))
-    GC.gc()
-end
-## swap out files...
-## read network from jlse dir, run with above params, save output
-for i in 1:3
-    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_I10_no_inc"*string(i)*".jlse", test(t; init_inf=I0, p_inf=pI))
-    GC.gc()
-end
-
-
-
-
-
-
-
-
-
-
-
-## network tests
 ## MD p=0.15
 xlim = floor(Int, 600/r_inc)
 xs = collect(1:xlim) .* r_inc;
@@ -436,6 +456,35 @@ plot(xs, hcat(ys_base,ys_no_inc),legend=nothing,
 
 
 0
+
+
+
+
+
+## initial infections
+
+p_by_cbg = dser_path("precalc/p_idxs_all_by_h_cbg.jlse")
+I0 = UInt32.(first(p_by_cbg["240230006001"], 100))
+
+for i in 2:3
+    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_iloc"*string(i)*".jlse", 
+    test(t; init_inf=I0, p_inf=pI, p_inf_hh=pI, p_inf_loc=pI,
+    distr_fn_hh=:const, distr_fn_non_hh=:const, distr_params_hh=(6,), distr_params_non_hh=(8,), 
+    distr_fn_loc_res=:exp, distr_params_loc_res=(2,), distr_fn_loc_work=:zero, distr_params_loc_work=() 
+    ))
+    GC.gc()
+end
+
+for i in 1:3
+    serialize("sim_MD_p0"*string(round(Int,pI*100))*"_h202_iloc"*string(i)*".jlse", 
+    test(t; init_inf=I0, p_inf=pI, p_inf_hh=pI, p_inf_loc=pI, holiday_time=202:223,
+    distr_fn_hh=:const, distr_fn_non_hh=:const, distr_params_hh=(6,), distr_params_non_hh=(8,), 
+    distr_fn_loc_res=:exp, distr_params_loc_res=(2,), distr_fn_loc_work=:zero, distr_params_loc_work=() 
+    ))
+    GC.gc()
+end
+
+
 
 
 
@@ -645,5 +694,18 @@ mean(g2)
 median(g2)
 std(g2)
 boxplot([rand(e2,100000) rand(g2,100000)])
+
+
+
+
+
+M = dser_path("jlse/adj_mat_wp.jlse");
+M = SparseMatrixCSC{Bool, UInt32}(Symmetric(M));
+wp_idxs = findnz(M)[1];
+ks = dser_path("jlse/adj_mat_keys.jlse");
+wp_keys = ks[wp_idxs]
+ppl = dser_path("jlse/people.jlse");
+x = DataFrame([(pk=k, com=ppl[k].commuter, age=ppl[k].age) for k in wp_keys if k in keys(ppl)]);
+minimum(x.age)
 
 
